825367f4093f0eb9d75f71276079e83d8153e892
fincap <finnkerman@gmail.com>
2022-05-03 22:19:07 +1000

    Implemented Pipeline header and stub source

--- src/core/Pipeline.cpp
+++ src/core/Pipeline.cpp
@@ -1,1 +1,14 @@
 #include "Pipeline.h"
+
+namespace mbc
+{
+  bool Pipeline::execute()
+  {
+    // TODO implement
+  }
+
+  bool Pipeline::addModule(ModulePtr)
+  {
+    // TODO implement
+  }
+}
--- src/core/Pipeline.h
+++ src/core/Pipeline.h
@@ -1,5 +1,28 @@
 #pragma once
-class Pipeline
-{
-};
+#include <vector>
+#include <typeindex>
+#include <memory>
+#include <unordered_map>
 
+#include "Module.h"
+#include "Payload.h"
+
+namespace mbc
+{
+  class Pipeline
+  {
+  public:
+    bool execute();
+    bool addModule(ModulePtr);
+
+  private:
+    // Modules by stage
+    std::vector<ModulePtr> generationModules_;
+    std::vector<ModulePtr> manipulationModules_;
+    std::vector<ModulePtr> renderModules_;
+    std::vector<ModulePtr> outputModules_;
+
+    std::unordered_map<std::type_index, PayloadPtr> payloads_;
+
+  };
+}
