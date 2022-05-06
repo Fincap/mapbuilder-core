30377108488b394f0527f6a0e817310eecf3bab6
fincap <finnkerman@gmail.com>
2022-05-06 21:07:04 +1000

    getValues added to Payload struct

--- src/core/Payload.h
+++ src/core/Payload.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <typeindex>
 #include <unordered_map>
+#include <any>
 #include <memory>
 
 namespace mbc
@@ -8,6 +9,9 @@ namespace mbc
   struct Payload
   {
     virtual ~Payload() = 0;
+
+    // Returns map of dervied class' payload values
+    virtual std::unordered_map<std::string, std::any> getValues() = 0;
   };
 
   // Inline definition required as a derived class will call the destructor of its base class.
--- src/payloads/Heightmap.h
+++ src/payloads/Heightmap.h
@@ -7,5 +7,17 @@ namespace mbc
   {
     int width, height;
     unsigned char* points;
+
+    std::unordered_map<std::string, std::any> getValues() override;
   };
+
+  inline std::unordered_map<std::string, std::any> Heightmap::getValues()
+  {
+    std::unordered_map<std::string, std::any> values;
+    values["width"] = width;
+    values["height"] = height;
+    values["points"] = points;
+
+    return values;
+  }
 }
