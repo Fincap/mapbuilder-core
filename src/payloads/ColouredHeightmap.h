134e6b5794f8c879a409b583afd35a0d66f33788
fincap <finnkerman@gmail.com>
2022-05-08 21:57:17 +1000

    Created ColouredHeightmap payload. Closes #25

---
+++ src/payloads/ColouredHeightmap.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <map>
+
+#include "..\core\Payload.h"
+
+namespace mbc
+{
+  // Payload storing a "rasterized" image of the generated map, based
+  // on the combination of the Heightmap and ColourSet payloads.
+  struct ColouredHeightmap : public Payload
+  {
+    int width, height;
+    uint32_t* colouredPoints_;
+
+  };
+}
