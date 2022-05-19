ebdad7b629e1b3ecccd27c7f25f01c2c951cab87
fincap <finnkerman@gmail.com>
2022-05-19 16:23:25 +1000

    Added util for accessing pointer to Payloads list. Contributes to #41

---
+++ src/util/ModuleHelpers.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <memory>
+
+#include "core\Payload.h"
+
+namespace mbc::util
+{
+  /*
+  Returns a shared_ptr to T in a PayloadTypeMap, where T is a type of Module.
+  */
+  template <typename T>
+  std::shared_ptr<T> getPtrToPayload(const PayloadTypeMap& payloads);
+
+  template<typename T>
+  inline std::shared_ptr<T> getPtrToPayload(const PayloadTypeMap& payloads)
+  {
+    // T must derive fom Payload
+    static_assert(std::is_convertible<T*, Payload*>::value, "Type must publicly inherit Payload");
+
+    // Create generic pointer to the payload within the payloads map
+    auto payload = payloads[std::type_index(typeid(T))];
+
+    // Cast generic pointer to type T and return
+    return std::dynamic_pointer_cast<T>(payload);
+  }
+}
