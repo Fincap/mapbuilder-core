585eb40d3c50484c15a31a8fef85cf3cef4030b1
fincap <finnkerman@gmail.com>
2022-05-23 17:34:47 +1000

    Created container class for types mapped to Stages.

---
+++ src/core/StageMap.h
@@ -0,0 +1,94 @@
+#pragma once
+#include "APIExport.h"
+
+#include <vector>
+#include <stdexcept>
+
+#include "core\Module.h"
+#include "core\PipelineStage.h"
+#include "util\ModuleHelpers.h"
+
+/*
+This templated class acts as a handle for a map of PipelineStages and a list of
+respective values. It is used to provide convenience for storing, iterating,
+and updating a list of objects based on their Pipeline stage.
+*/
+template <typename T>
+class MAPBUILDER_API StageMap
+{
+public:
+  // Iterator types
+  using iterator = T*;
+  using const_iterator = T const*;
+
+  StageMap();
+
+  void add(T, mbc::PipelineStage);  // Add T to given stage.
+  void clear();                     // Clear all values.
+
+  std::vector<T> getAll(mbc::PipelineStage);  // Get all in given stage.
+  std::vector<T> getAll(int);                 // Get all in given stage.
+  std::vector<T> getAll();                    // Get all in all stages.
+
+  // Iterator functions
+  iterator begin() { return &map_[0]; }
+  iterator end() { return &map_[MBC_NUM_STAGES]; }
+  const_iterator begin() const { return &map_[0]; }
+  const_iterator end() const { return &map_[MBC_NUM_STAGES]; }
+
+private:
+  std::vector<T>* map_;
+
+};
+
+
+template<typename T>
+inline StageMap<T>::StageMap()
+{
+  map_ = new std::vector<T>[MBC_NUM_STAGES];
+}
+
+
+template<typename T>
+inline void StageMap<T>::add(T value, mbc::PipelineStage stage)
+{
+
+}
+
+
+template<typename T>
+inline void StageMap<T>::clear()
+{
+
+}
+
+
+template<typename T>
+inline std::vector<T> StageMap<T>::getAll(mbc::PipelineStage stage)
+{
+  return getAll((int) stage);
+}
+
+
+template<typename T>
+inline std::vector<T> StageMap<T>::getAll(int stage)
+{
+  if (stage >= MBC_NUM_STAGES)
+    throw std::out_of_range;
+  return map_[stage];
+}
+
+
+template<typename T>
+inline std::vector<T> StageMap<T>::getAll()
+{
+  std::vector<T> all;
+
+  for (int stage = 0; stage < MBC_NUM_STAGES; stage++)
+  {
+    for (auto& val : map_[stage])
+      all.push_back(val);
+  }
+
+  return all;
+}
